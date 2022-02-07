1e3f0e0e753a4e43bf37f6fbd05e793a33c3d8f7
ClaesF0 <claes.folkestad@stud.noroff.no>
2022-02-07 10:18:45 +0100

    js lesson 1 notes

---
+++ js/script.js
@@ -0,0 +1,49 @@
+/*
+const count = 1;
+count = 99; 
+count = count + 99;
+*/
+/*
+let count = 1; 
+count = 2;
+//count = count + 1;
+count = count +1;
+count++// add 1
+count+2// add 2
+count+3// add 3
+
+console.log("my count is equal to: ", count)
+
+{
+    let someVar = "dog"
+    function bark (){
+        console.log(pet)
+    }
+    bark();
+}
+*/
+
+//var og let har forskjellige egenskaper, hvor de kan/ikke kan brukes, sjekk når du forstår mer
+
+const profile = {
+    name: "Mats",
+    age: "28",
+    job: "Doctor"
+}
+
+console.log(profile) 
+console.dir(profile)
+
+//const sentences = ` First sentence.
+//                    Second sentence `
+// Å bruke ` `  eller backslash vil gi oss en enkel måte å dele strings over linjer, for å få kode mer leselig.
+
+
+let name = "Hesh";
+console.log(name === true)
+
+console.log (profile["name"]);
+
+const dog = {}
+
+// DOM = Document Object Model
